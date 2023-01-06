3543435ae334c9af62b416d9c0b97a5ca9b41ab1
shifu <skij99@gmail.com>
2023-01-06 18:34:06 -0500

    Added README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# K-Means
+
+Clustering using K-Means.
+Result of KMeans using K = 3.
+![KMeans](https://github.com/skij487/K-Means/blob/main/images/clusters.svg)
+
+## How to Run
+Clone repository and run demo.py
+```console
+foo@bar:~$ python demo.py
+```
--- demo.py
+++ demo.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import KMeans
 
-points = np.loadtxt('POIs')
+points = np.loadtxt('data/POIs')
 K = 3
 zs, cost, mus = KMeans.KMeansMin(points, K)
 colors = [(1,0,0), (0,1,0), (0,0,1)]
 for i in range(K):
         plt.plot(points[zs == i,0],points[zs == i,1],'.',color=colors[i],markersize=8)
         plt.plot(mus[i][0],mus[i][1],'k*',markersize=16)
+plt.savefig('images/clusters.svg')
 plt.show()
